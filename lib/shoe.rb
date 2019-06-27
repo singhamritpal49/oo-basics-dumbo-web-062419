4e731a96b1716b37af6729159473354e01371e2d
Your Name <you@example.com>
2019-06-26 20:42:20 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
-# Make your shoe class here!
+require 'pry'
+class Shoe
+
+  
+    attr_accessor :color, :size, :material, :condition
+
+    attr_reader :brand
+    def initialize(brand)
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+    
+
+
+end
